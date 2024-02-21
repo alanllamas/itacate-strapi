7b191ed1d563a339ca6e63ecae1d5f7c79858a9c
alan llamas <alanllamasg@gmail.com>
2024-02-20 21:41:32 -0600

    feat: added postgress database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,24 @@
-const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  // console.group('database1 :');
+  // console.log('DATABASE_HOST_PRODUCTION: ', env('DATABASE_HOST_PRODUCTION'));
+  // console.log('DATABASE_PORT_PRODUCTION: ', env('DATABASE_PORT_PRODUCTION'));
+  // console.log('DATABASE_NAME: ', env('DATABASE_NAME'));
+  // console.log('DATABASE_USER_PRODUCTION: ', env('DATABASE_USER_PRODUCTION'));
+  // console.log('DATABASE_PASS_PRODUCTION: ', env('DATABASE_PASS_PRODUCTION'));
+  // console.log('DATABASE_SSL: ', env('DATABASE_SSL') === 'true');
+  // console.groupEnd();
+  return ({
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST_PRODUCTION'),
+        port: env('DATABASE_PORT_PRODUCTION'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USER_PRODUCTION'),
+        password: env('DATABASE_PASS_PRODUCTION'),
+        ssl: env('DATABASE_SSL') === 'true'
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+  })
+};
